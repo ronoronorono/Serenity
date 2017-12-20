18f6c1cc0f28e190cdc9b88653d0dd51f18061ac
rono <antoniovitor.rezende@gmail.com>
2017-12-20 18:02:26 -0200

    removed unnecessary uses

--- RonoBot/Modules/Admin.cs
+++ RonoBot/Modules/Admin.cs
@@ -4,10 +4,7 @@ using Discord.Rest;
 using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 
--- RonoBot/Modules/Util.cs
+++ RonoBot/Modules/Util.cs
@@ -1,15 +1,9 @@
 ﻿using Discord;
 using Discord.Commands;
-using Discord.Rest;
 using Discord.WebSocket;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Threading;
 
 namespace RonoBot.Modules
 {
