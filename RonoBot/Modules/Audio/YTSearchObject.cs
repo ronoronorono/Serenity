cfd56ce9b3eb56c963436797ce3ddffdb23c2091
rono <antoniovitor.rezende@gmail.com>
2018-01-25 08:58:47 -0200

    added new class object to handle yt searches

---
+++ RonoBot/Modules/Audio/YTSearchObject.cs
@@ -0,0 +1,25 @@
+﻿using Google.Apis.YouTube.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RonoBot.Modules.Audio
+{
+    public class YTSearchObject
+    {
+        SearchResult ytresult;
+        string query;
+        int order;
+
+        public YTSearchObject(SearchResult ytresult, string query, int order)
+        {
+            this.ytresult = ytresult;
+            this.query = query;
+            this.order = order;
+        }
+
+        public SearchResult Ytresult { get => ytresult; set => ytresult = value; }
+        public string Query { get => query; set => query = value; }
+        public int Order { get => order; set => order = value; }
+    }
+}
