8dda44b214975e1e0be21d9fe1f257e136ab5360
rono <antoniovitor.rezende@gmail.com>
2018-02-24 17:59:49 -0300

    added new search method

--- RonoBot/Modules/Searches.cs
+++ RonoBot/Modules/Searches.cs
@@ -20,8 +20,8 @@ namespace RonoBot.Modules
     {
         //Searches for an image through the google search engine
         //selecting a random image from the 10 first found
-        [Command("img")]
-        public async Task SearchImgAsync([Remainder] string search)
+        [Command("rimg")]
+        public async Task SearchRImgAsync([Remainder] string search)
         {
  
             var customSearchService = new CustomsearchService(new BaseClientService.Initializer { ApiKey = SerenityCredentials.GoogleAPIKey() });
@@ -50,6 +50,38 @@ namespace RonoBot.Modules
                    .WithImageUrl(paging.ElementAt(index).Link);
 
             await Context.Channel.SendMessageAsync("",false,embedImg);
+        }
+
+
+        //Same as above but gets the first image instead.
+        [Command("img")]
+        public async Task SearchImgAsync([Remainder] string search)
+        {
+
+            var customSearchService = new CustomsearchService(new BaseClientService.Initializer { ApiKey = SerenityCredentials.GoogleAPIKey() });
+
+            var listRequest = customSearchService.Cse.List(search);
+            Random rnd = new Random();
+            listRequest.Cx = SerenityCredentials.CustomSearchEngineKey();
+
+            //Restricting the search to only images
+            listRequest.SearchType = CseResource.ListRequest.SearchTypeEnum.Image;
+
+            //The results will be put inside this list
+            IList<Result> paging = new List<Result>();
+
+            //Begins the search
+            paging = listRequest.Execute().Items;
+
+            int index = 0;
+
+            var embedImg = new EmbedBuilder()
+                   .WithColor(new Color(240, 230, 231))
+                   .WithAuthor(eab => eab.WithName(search))
+                   .WithDescription(paging.ElementAt(index).Link)
+                   .WithImageUrl(paging.ElementAt(index).Link);
+
+            await Context.Channel.SendMessageAsync("", false, embedImg);
         }
     }
 }
