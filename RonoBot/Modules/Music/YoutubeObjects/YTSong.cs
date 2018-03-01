82777138e4fce83184d1d5b4e837ded6f265b5ea
rono <antoniovitor.rezende@gmail.com>
2018-03-01 04:15:20 -0300

    Now uses a proper constructor

--- RonoBot/Modules/Music/YoutubeObjects/YTSong.cs
+++ RonoBot/Modules/Music/YoutubeObjects/YTSong.cs
@@ -11,42 +11,83 @@ namespace RonoBot.Modules.Audio
 {
     public class YTSong
     {
-        SearchResult ytresult;
-        string query;
-        int order;
-        //Not to be confounded with the video's author, this actually references the user who
-        //requested the song
-        SocketUser requestAuthor;
-        string duration;
+        private string title;
+        private string defaultThumbnailUrl;
+        private string videoID;
+        private string audioURI;
+        private string url;
+        private string query;
+        private int order;
+        private SocketUser requestAuthor;
+        private string duration;
 
-        public YTSong(SearchResult ytresult, string query, int order, SocketUser requestAuthor)
+        public string Title { get => title; set => title = value; }
+        public string DefaultThumbnailUrl { get => defaultThumbnailUrl; set => defaultThumbnailUrl = value; }
+        public string VideoID { get => videoID; set => videoID = value; }
+        public string Query { get => query; set => query = value; }
+        public int Order { get => order; set => order = value; }
+        public SocketUser RequestAuthor { get => requestAuthor; set => requestAuthor = value; }
+        public string Duration { get => duration; set => duration = value; }
+        public string Url { get => url; set => url = value; }
+        public string AudioURI { get => audioURI; set => audioURI = value; }
+
+        public YTSong(string title, string defaultThumbnailUrl, string videoID, string audioURI, string query, int order, SocketUser requestAuthor, string duration)
         {
-            this.ytresult = ytresult;
+            this.title = title;
+            this.defaultThumbnailUrl = defaultThumbnailUrl;
+            this.videoID = videoID;
+            this.url = "https://www.youtube.com/watch?v=" + videoID;
+            this.audioURI = audioURI;
             this.query = query;
             this.order = order;
             this.requestAuthor = requestAuthor;
+            this.duration = duration;
+        }
 
-            this.duration = YTVideoOperation.GetVideoDuration(ytresult.Id.VideoId);
+        public YTSong(Video video,string query, int order, SocketUser requestAuthor)
+        {
+            this.title = video.Snippet.Title;
+            this.defaultThumbnailUrl = video.Snippet.Thumbnails.Default__.Url;
+            this.videoID = video.Id;
+            this.url = "https://www.youtube.com/watch?v=" + video.Id;
+
+            string uri = YTVideoOperation.GetVideoURIExplode(video.Id).Result;           
+            if (uri == null)
+                uri = YTVideoOperation.GetVideoAudioURI(this.url);
+
+
+            this.audioURI = uri;
+            this.query = query;
+            this.order = order;
+            this.requestAuthor = requestAuthor;
+            this.duration = YTVideoOperation.FormatVideoDuration(video.ContentDetails.Duration);
+        }
+
+        public YTSong(SearchResult ytresult, string audioURI, string query, int order, SocketUser requestAuthor, string duration)
+        {
+            this.title = ytresult.Snippet.Title;
+            this.defaultThumbnailUrl = ytresult.Snippet.Thumbnails.Default__.Url;
+            this.videoID = ytresult.Id.VideoId;
+            this.url = "https://www.youtube.com/watch?v=" + videoID;
+            this.audioURI = audioURI;
+            this.query = query;
+            this.order = order;
+            this.requestAuthor = requestAuthor;
+            this.duration = duration;
         }
 
         public YTSong(YTSong song)
         {
-            this.ytresult = song.Ytresult;
+            this.title = song.Title;
+            this.defaultThumbnailUrl = song.DefaultThumbnailUrl;
+            this.videoID = song.VideoID;
+            this.url = song.Url;
+            this.audioURI = song.AudioURI;
             this.query = song.Query;
             this.order = song.Order;
             this.requestAuthor = song.RequestAuthor;
             this.duration = song.Duration;
         }
-
-        public SearchResult Ytresult { get => ytresult; set => ytresult = value; }
-        public string Query { get => query; set => query = value; }
-        public int Order { get => order; set => order = value; }
-        public SocketUser RequestAuthor { get => requestAuthor; set => requestAuthor = value; }
-        public string Duration { get => duration; set => duration = value; }
-        public string GetUrl ()
-        {
-            return "https://www.youtube.com/watch?v=" + Ytresult.Id.VideoId;
-        }
-
+        
     }
 }
